5ceb0d278622aba7ae03d4345d9d94a38c2785c2
portion1929 <santiago.lagarde@ucas-edu.net>
2025-01-29 09:53:35 -0700

    pracyice

---
+++ Variables/practice.py
@@ -0,0 +1,32 @@
+#Santiago Lagarde Variables Practice Py
+
+
+name = ("Santiago")
+print(name)
+
+u = ("Ucas")
+print(u)
+
+y = ("2025")
+print(y)
+
+ec = ("brown")
+print(ec)
+
+n1 = ("5")
+print(n1)
+
+n100 = ("250")
+print(n100)
+
+f = ("HotChoco")
+print(f)
+
+c = ("purple")
+print(c)
+
+a = ("14")
+print(a)
+
+s = ("DigiMedia")
+print(s) 
