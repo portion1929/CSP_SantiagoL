f64f93fa7499c0a4b7b6973049cb16db1b6afaea
portion1929 <santiago.lagarde@ucas-edu.net>
2025-01-31 09:31:26 -0700

    practice c

---
+++ Variables/practice.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+
+char name[] = "Santiago";
+int snum = 5;
+int bnum = 200;
+char food[] = "sandwich"; 
+char color[] = "purple";
+char sch[] = "UCAS";
+int year = 2025;
+char eye[] = "brown";
+int age = 14;
+char sub[] = "DigiMedia";
+
+int main (void){
+printf("%s\n", name);
+printf("%d\n", snum);
+printf("%d\n", bnum);
+printf("%s\n", food);
+printf("%s\n", color);
+printf("%s\n", sch);
+printf("%d\n", year);
+printf("%s\n", eye);
+printf("%d\n", age);
+printf("%s\n", sub);
+
+return(0);
+}
