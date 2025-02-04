15c104686c5d21ba6647a0fed8c132b9351265dd
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-04 09:35:12 -0700

    pi

--- expressions/notes.py
+++ expressions/notes.py
@@ -2,13 +2,19 @@
 #integers = numbers without decimal
 #floats = number with decimal
 #algorithms = set of instructions to complete a task
-print(10.5+5)
-print(10.5-5)
-print(10.5*5)
-print(10.5/5)
-print(10.5**5) #exponents
-print(10.5//5) #whole numb div
-print(10.5%5) #modulo/modulus/mod
+#print(10.5+5)
+#print(10.5-5)
+#print(10.5*5)
+#print(10.5/5)
+#print(10.5**5) #exponents
+#print(10.5//5) #whole numb div
+#print(10.5%5) #modulo/modulus/mod
 
-print(3+7*2)
-print((3+7)*2*2)
+#print(3+7*2)
+#print((3+7)*2*2)
+x = 2
+print(x)
+
+x %= 26.2
+
+print(x)
