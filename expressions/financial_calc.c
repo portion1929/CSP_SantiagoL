46f93c95aa29d2793caa6d7d89e03da01bf6eca2
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-07 09:52:29 -0700

    finance c

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -2,8 +2,30 @@
 
 #include <stdio.h> 
 #include <math.h>
+float income;
+float rent;
+float utilities;
+float groceries;
+float transport;
+
+float percent_rent;
+float percent_utilities;
+float percent_groceries;
+float percent_transport;
 
 int main(void){
-printf("");
+printf("This calculator will help you deal with Finances.");
+printf("How much do you make monthly?");
+scanf("%f", &income);
+printf("How much do you spend on Rent?");
+scanf("%f", &rent);
+printf("How much do you spend on Utilities?");
+scanf("%f", &utilities);
+printf("How much do you spend on Groceries");
+scanf("%f", &groceries);
+printf("How much do you spend on Transport?");
+scanf("%f", &transport);
+float savings = income/90;
+float spending_money = income - (rent+utilities+groceries+transport+savings);
     return 0;
 }
