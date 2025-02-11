5f3c34d6ab594a1b039cf2a65e9f2dbd01f3a918
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-11 09:35:05 -0700

    fix

--- Strings/notes.c
+++ Strings/notes.c
@@ -9,7 +9,11 @@ int main(void){
 //scanf("%s", name);
 //fgets(name, 20, stdin);
 //printf("Hello %s, welcome to my proga", name);
-char sentence[] = "The quick black fox jumps over the white dog";
-printf("%lu\n", sizeof(sentence));
+//char sentence[] = "The quick black fox jumps over the white dog";
+//printf("%lu\n", strlen(sentence));
+ char one[] = "Hello ";
+ char two[] = "World";
+ strcat(one, two);
+ printf("%s\n", one);
     return 0;
 }
