f4eee1d6d9d51514ee087bc82acd2c866f5f41fd
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-12 09:20:56 -0700

    d

---
+++ Strings/name.dec.c
@@ -0,0 +1,14 @@
+//santiago L, name dec
+
+#include <stdio.h> 
+#include <string.h>
+
+char name[] = "Hello ";
+char decor1[] = "<------>";
+char decor2[] = "<-----> ";
+
+
+int main(void){
+printf("Hello World");
+    return 0;
+}
