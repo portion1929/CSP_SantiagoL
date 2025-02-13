b4ca091fcaac7a96f75ad4c3f2cf5054bbe652ea
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-13 09:52:24 -0700

    stf

--- Strings/sillysentences.py
+++ Strings/sillysentences.py
@@ -1,1 +1,8 @@
 #santiago lagarde silly snetences
+
+name = input("Welcome to the Silly Sentence Machine! this program will let you make funny sentences for you and your friends! To start, please type in your name.")
+
+friend = input("Hello, now please type in your favorite friend.")
+verb = input("Now, type in your favorite verb.")
+adj = input("Great! now type in your favorite adjective")
+print(f (name))
