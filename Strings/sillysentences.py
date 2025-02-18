a8cc780b2c0e2dcf6106f25f1d45e9f2f15fcf50
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-18 09:20:37 -0700

    done

--- Strings/sillysentences.py
+++ Strings/sillysentences.py
@@ -1,8 +1,11 @@
 #santiago lagarde silly snetences
 
-name = input("Welcome to the Silly Sentence Machine! this program will let you make funny sentences for you and your friends! To start, please type in your name.")
-
-friend = input("Hello, now please type in your favorite friend.")
-verb = input("Now, type in your favorite verb.")
-adj = input("Great! now type in your favorite adjective")
-print(f"while"(name), "was walking from his"(adj) (friend),(verb), "into him.")
+name = input("What is your name?")
+print(f"hello {name}, this program will let you put in words that make silly sentences!")
+verb = input("Please give me a Verb.")
+verb2 = input("Give another verb.")
+area = input("What place do you go to the most?")
+adjective = input("What is your favorite adjective?")
+person = input("Who is your favorite person?")
+print("And now, your new sentence is...")
+print(f"While you were {verb} in a {area}, you stumbled into a {adjective} man, but luckily, {person} who was {verb2} in the same {area} that you were in saved you.")
---
+++ functions/notes.py
@@ -0,0 +1,8 @@
+#Santiago Lagarde, function notes
+
+#functions = action stored in key word
+
+def add():
+    numOne = int(input("give a number:\n"))
+    numTwo = int(input("give another number:\n"))
+    print(numOne+numTwo)
