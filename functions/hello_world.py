452a9fbb1a2346a704354ba5bd49d3ed88f69b77
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-19 09:35:53 -0700

    asd

---
+++ functions/hello_world.py
@@ -0,0 +1,16 @@
+#santiago lagarde hello world upd
+
+
+def user(word):
+    type = input(f"tell me your {word}")
+    return type
+name = user("name")
+name2 =user("friends name")
+name3 =user("grandmas name")
+name4 =user("pets name")
+name5 =user("moms name")
+print(f"{name}, say hello to the world!")
+print(f"{name2}, say hello to the world!")
+print(f"{name3}, say hello to the world!")
+print(f"{name4}, say hello to the world!")
+print(f"{name5}, say hello to the world!")
