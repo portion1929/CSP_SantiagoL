b01930de1067a00df62a9be5a248f68bc52512fc
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-20 09:53:08 -0700

    asddsa

--- functions/hello_world.c
+++ functions/hello_world.c
@@ -1,6 +1,10 @@
 #include <stdio.h> 
+char name1[50] = "Michael";
+char name2[50] = "Joanne";
+char name3[50] = "Tyler";
+char name4[50] = "The";
+char name5[50] = "Creator";
+int name(void){
 
-void name("what"){
-
- 
+ return 0;
 }
