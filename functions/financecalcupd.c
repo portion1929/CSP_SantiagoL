98d81c7fc1343578786d9a69e2e42cb494e243bc
portion1929 <santiago@ucas-edu.net>
2025-02-23 15:20:19 -0700

    asd

--- functions/financecalcupd.c
+++ functions/financecalcupd.c
@@ -1,9 +1,44 @@
 //santiago L, finance calc c
 
-#include <stdio.h> 
+#include <stdio.h>
+float income, rent, utilities, groceries, transport, savings, used, expenses;
 
-printf("Welcome to the Finance Calculator!")
+float input(char type[], float var){
+    printf("what is your monthly %s?:\n", type);
+    scanf("%f", &var);
+    return var;
+}
 
-int user(int type[50]){
-    printf("how much do you spend on %s\n",type);
+void percent(char type[], int ammount){
+    int per = ammount/income * 100;
+
+    printf("your %s is %d%% of your income.\n", type, per);
+}
+    
+int main(void){
+    
+    printf("This calculator will find out all of your expenses for you.\n");
+    income = input("income", income);
+    rent = input("rent", rent);
+    utilities = input("utilities", utilities);
+    groceries = input("groceries", groceries);
+    transport = input("transportation", transport);
+    
+    savings = income *.2;
+    expenses = rent + utilities + groceries + transport;
+    used = income - expenses - savings;
+    
+    printf("you make %.2f\n", income);
+    printf("so, your expenses are $%.2f\n", expenses);
+    printf("and your savings are $%.2f\n", savings);
+    printf("as well your spending money is $%.2f\n", used);
+
+    percent("rent", rent);
+    percent("utilities", utilities);
+    percent("groceries", groceries);
+    percent("transportation", transport);
+    percent("savings", savings); 
+    percent("expenses", expenses);
+    percent("spending", used);   
+    return 0;
 }
