afe6de16b0e0ba8129c93d756d92a464cdf56fa0
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-24 09:51:36 -0700

    d

--- conditionals/notes.py
+++ conditionals/notes.py
@@ -11,19 +11,26 @@ print("helllo walecoe to program that conditional")
 #else:
     #print("your student, wec calass")
 
-num = input("How many would you like:(Give number 1-10)\n ")
+#num = input("How many would you like:(Give number 1-10)\n ")
 #only takes first condition
 #elif checks another number variable thing
-if num != 1:
-    print("You have asked for item")
-elif num <= 3:
-    print("you hav e aske dfor a couple items")
-elif num <= 5:
-    print("you have asked for a few othe the items.. or you in south and have asked for a couple")
-else: 
-    print("you have asked for some of item")
+#if num != 1:
+    #print("You have asked for item")
+#elif num <= 3:
+    #print("you hav e aske dfor a couple items")
+#elif num <= 5:
+    #print("you have asked for a few othe the items.. or you in south and have asked for a couple")
+#else: 
+    #print("you have asked for some of item")
 
 # <= IS LESS or EQUAL
 # >= IS MORE or EQUAL
 #in is in the string
 #! is not in the strng
+
+name = "ma"
+
+if "a" in name or "e" in name or "i" in name or "o" in name or "u" in name:
+    print("your name has vowl")
+else:
+    print("your name does not hvae vowl gng")
