707b2d4484fe266ae863ce0d89ae09a6cbaec6b0
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-24 09:39:34 -0700

    asd

--- conditionals/notes.py
+++ conditionals/notes.py
@@ -11,10 +11,10 @@ print("helllo walecoe to program that conditional")
 #else:
     #print("your student, wec calass")
 
-num = 1
+num = input("How many would you like:(Give number 1-10)\n ")
 #only takes first condition
 #elif checks another number variable thing
-if num == 1:
+if num != 1:
     print("You have asked for item")
 elif num <= 3:
     print("you hav e aske dfor a couple items")
@@ -22,3 +22,8 @@ elif num <= 5:
     print("you have asked for a few othe the items.. or you in south and have asked for a couple")
 else: 
     print("you have asked for some of item")
+
+# <= IS LESS or EQUAL
+# >= IS MORE or EQUAL
+#in is in the string
+#! is not in the strng
