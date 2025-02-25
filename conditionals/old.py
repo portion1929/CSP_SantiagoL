cba0deca0c57ae4e9aa5a513f5077d909a732ef2
santiago.lagarde <santiago.lagarde@ucas-edu.net>
2025-02-25 09:52:34 -0700

    asdads

---
+++ conditionals/old.py
@@ -0,0 +1,5 @@
+age = input("how old are you?")
+if age >5:
+    print(f"because you are {age}, you are old enough to go to school")
+else: 
+    print(f"because you are {age}, you are not allowed to go to school")
