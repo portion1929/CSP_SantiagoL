33d5f31d5a8965aa1202b1fa4a321535f913943e
portion1929 <santiago.lagarde@ucas-edu.net>
2025-02-28 09:18:21 -0700

    asdada

--- conditionals/oldassignment.c
+++ conditionals/oldassignment.c
@@ -1,2 +1,25 @@
-  #include <stdio.h>
-  #include <time.h>
+#include <stdio.h> 
+#include <time.h> 
+int hour;
+
+int main ()
+{
+    time_t rawtime;
+    struct tm *timeinfo; 
+
+    time(&rawtime); 
+    timeinfo = localtime (&rawtime); 
+    printf ("tell me the hour in military time (like 1-23):\n"); 
+    scanf("%d", &hour); 
+    if (hour <  12 && hour >=5){
+    printf("Good Morning."); 
+    }else if (hour<=18 && hour >=12){
+        printf("Good Afternoon"); 
+    }else if (hour>=19 && hour <=21){
+        printf ("Good Evening.");
+    }else{
+      printf("Good Night.");
+    }
+    
+    return 0; 
+}
