e718b03cb889b7c5d2aca87fd3314fb9e7160818
portion1929 <santiago.lagarde@ucas-edu.net>
2025-03-06 09:13:30 -0700

    asdasda

---
+++ loops/fam.py
@@ -0,0 +1,6 @@
+#santiago L, family loop py
+num = 0
+siblings = ["Alex", "Katie", "Andrew","Ryan", "Dion", "Emma","Sebastian"]
+for sibling in siblings:
+    print(f"{num}. {sibling} Lagarde")
+    num += 1
