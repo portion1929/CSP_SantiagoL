5949f94edb6f56e95126dfbfc011f9e3b3354c4f
portion1929 <santiago.lagarde@ucas-edu.net>
2025-03-07 09:26:27 -0700

    asdadada

--- loops/buzz.py
+++ loops/buzz.py
@@ -2,11 +2,11 @@
 nums = 1
 while nums >= 1 and nums <51:
     if nums % 15 == 0:
-        print("Fizz\n")
+        print("Fizz.\n")
     elif nums % 5 == 0:
-        print("Buzz\n")
+        print("Buzz.\n")
     elif nums % 3 == 0:
-        print("FizzBuzz\n")
+        print("FizzBuzz.\n")
     else:
         print(nums)
     nums += 1
