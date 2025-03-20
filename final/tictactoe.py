b8277791f329d14cd64b742912d79632bab01a5b
portion1929 <santiago.lagarde@ucas-edu.net>
2025-03-20 15:33:23 +0000

    sadadsasdads

--- final/tictactoe.py
+++ final/tictactoe.py
@@ -1,11 +1,7 @@
-board = [[1,2,3], [4,5,6], [7,8,9]]
-rows =3
-cols = 3
+#grid
+grid = [[1,2,3],
+[4,5,6],
+[7,8,9]]
 
-def gameboard():
-    for x in range(rows):
-        print("+---+---+---+")
-        print("|", end ="")
-        for y in range(cols):
-            print("", gameboard[x][y], end="|")
-    print("\n+---+---+---+")
+for grid in grid:
+    print(f" {grid[0]} | {grid[1]} | {grid[2]}")
