a9aaaddf6621ab30091fdad8a18d4803dc36e9a9
portion1929 <santiago.lagarde@ucas-edu.net>
2025-03-28 15:48:55 +0000

    asdasdads

--- final/tictactoe.py
+++ final/tictactoe.py
@@ -1,8 +1,146 @@
-#grid
-grid = [[1,2,3],
-[4,5,6],
-[7,8,9]]
+# Ryan Crop, Final project
 
-for grid in grid:
-    print(f" {grid[0]} | {grid[1]} | {grid[2]}\n --+---+--")
+#Santiagos first part, other parts are with the grids
+grid = [['1','2','3'],
+['4','5','6'],
+['7','8','9']]
+name = input("What is your name?\n") #These two lines where part of Johanns 
+print(f"Hello {name}. This is a game of tic tac toe but with a small bit of extra rules.\n First the bot gets to go first because of how hard he works each day.\n Next is if you attempt to put your X on a spot already occupied, then you will get your turn skipped as punishment.\n Finally if you win you will get a two chant win cheer.\n")
 
+#First part of Ryan Crops
+import random
+
+game_finished = False
+
+repeat = 1
+taken_list = []
+#Ryan R's part
+def win(grid, game_finished):
+
+    if   grid[0][0] == "X" and grid[0][1]=="X" and grid[0][2]=="X":
+         print("X wins")
+         game_finished = True
+        
+    elif grid[1][0] == "X" and grid[1][1]=="X" and grid[1][2]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[2][0] == "X" and grid[2][1]=="X" and grid[2][2]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[0][0] == "O" and grid[0][1]=="O" and grid[0][2]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[1][0] == "O" and grid[1][1]=="O" and grid[1][2]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[2][0] == "O" and grid[2][1]=="O" and grid[2][2]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[0][0] == "X" and grid[1][0]=="X" and grid[2][0]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[0][0] == "X" and grid[1][1]=="X" and grid[2][2]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[0][2] == "X" and grid[1][2]=="X" and grid[2][2]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[0][0] == "X" and grid[1][1]=="X" and grid[2][2]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[0][2] == "X" and grid[1][1]=="X" and grid[2][0]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[0][1] == "X" and grid[1][1]=="X" and grid[2][1]=="X":
+        print("X wins")
+        game_finished = True
+    elif grid[0][0] == "O" and grid[1][0]=="O" and grid[2][0]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[0][0] == "O" and grid[1][1]=="O" and grid[2][2]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[0][2] == "O" and grid[1][2]=="O" and grid[2][2]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[0][0] == "O" and grid[1][1]=="O" and grid[2][2]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[0][2] == "O" and grid[1][1]=="O" and grid[2][0]=="O":
+        print("O wins")
+        game_finished = True
+    elif grid[0][1] == "O" and grid[1][1]=="O" and grid[2][1]=="O":
+        print("O wins")
+        game_finished = True
+    elif all(spot in ["X", "O"] for row in grid for spot in row):
+        print("It's a tie.")
+        game_finished = True
+    return game_finished
+
+#Ryan Crops part
+while game_finished == False:
+    random_integer = random.randint(1, 9)
+    game_finished = win(grid, game_finished)  
+    
+    if random_integer in taken_list:
+        continue
+    else:
+        taken_list.append(random_integer)
+        if random_integer == 1:
+            grid[0][0] ="O" 
+        elif random_integer == 2: 
+            grid[0][1] ="O"
+        elif random_integer == 3: 
+            grid[0][2] ="O"
+        elif random_integer == 4: 
+            grid[1][0] ="O"
+        elif random_integer == 5: 
+            grid[1][1] ="O"
+        elif random_integer == 6: 
+            grid[1][2] ="O"
+        elif random_integer == 7: 
+            grid[2][0] ="O"
+        elif random_integer == 8: 
+            grid[2][1] ="O"
+        elif random_integer == 9: 
+            grid[2][2] ="O"
+        for list in grid:
+            print(f" {list[0]} | {list[1]} | {list[2]}\n --+---+--")
+        win(grid, game_finished) 
+        if game_finished is True:
+            break
+        else:
+            print("")
+            
+    
+#Johanns part
+    if game_finished == False:
+        spot = int(input("pick a number for your spot 1-9 single digit please. (If games already over then it's because you lost and the bot feels bad for you so it's giving you a freebie.)\n"))
+
+    if spot in taken_list:
+        print("That spot is taken or not on the list, because you didn't listen to the rules your turn is skipped >:) !")
+        continue
+    else: 
+        taken_list.append(spot)
+        if spot == 1:
+            grid[0][0]="X"
+        elif spot == 2:
+            grid[0][1] ="X"
+        elif spot == 3:
+            grid[0][2] ="X"
+        elif spot == 4:
+            grid[1][0]="X"
+        elif spot == 5: 
+            grid[1][1]="X"
+        elif spot == 6:
+            grid[1][2] ="X"
+        elif spot == 7:
+            grid[2][0] ="X"
+        elif spot == 8:
+            grid[2][1] ="X"
+        elif spot == 9:
+            grid[2][2] ="X"
+#Ryan Rs part
+    win(grid, game_finished) 
+    if game_finished is True:
+        break
--- functions/finanacecalcupd.py
+++ functions/finanacecalcupd.py
@@ -1,26 +1,27 @@
-#santiago lagarde finance calc py
-print("This calculator will help you deal with Finances.\n")
+# Santiago Lagarde, Financial Calculator Python
+print("This calculator will help you calculate your financial situations\n")
 def info(income, amount,type):
     pertype = (amount/income)*100
-    print(f"You spend ${amount:.2f} on {type} and that is {pertype:.0f}% of your income.")
+    print(f"You spent ${amount:.2f} on {type} and that is {pertype:.0f}% of your income.")
 
 def questions(inputs):
     questions = float(input(f"What is your monthly {inputs}?\n"))
    
     return questions
 
-income= input ("what is your monthly income?\n")
-rent = input ("what is your monthly rent cost?")
-utilities = input ("what is your monthly utilities cost?\n")
-groceries = input ("what is your monthly grocery cost?\n")
-transport = input ("what is your monthly transport cost?\n")
-savings = income*2
-expenses = rent+ utilities + groceries + transport
-spending = income -expenses -savings
+income= float(input ("what is your monthly income?\n"))
+rent = float(input ("what is your monthly rent cost?\n"))
+utilities = float(input ("what is your monthly utilities cost?\n"))
+groceries = float(input ("what is your monthly grocery cost?\n"))
+transport = float(input ("what is your monthly transport cost?\n"))
+savings = float(income/10)
+expenses = float(rent+utilities+groceries+transport+savings)
+spending = float(income-expenses)
 
-info(income, rent, "rent")
-info(income, utilities, "utilities")
-info(income, transport, "transport")
-info(income, savings, "savings")
-info(income, spending, "spending")
-info(income, groceries, "groceries")
+info(income, rent, "Rent")
+info(income, utilities, "Utilities")
+info(income, transport, "Transport")
+info(income, savings, "Savings")
+info(income, expenses, "Expenses")
+info(income, spending, "Spending")
+info(income, groceries, "Groceries")
