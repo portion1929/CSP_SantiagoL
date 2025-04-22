724a60dff278403168f77458704f2682f3f7a7c2
portion1929 <santiago.lagarde@ucas-edu.net>
2025-04-22 09:46:40 -0600

    asdadsads

--- javascript/basic.js
+++ javascript/basic.js
@@ -1,5 +1,27 @@
+let images = ["https://m.media-amazon.com/images/I/61Jr-rvLUDL._UF1000,1000_QL80_.jpg", "https://m.media-amazon.com/images/I/718khrgr5bL._UF1000,1000_QL80_.jpg"]
+
+
 document = notes.html
 function hello(){
-    document.getElementById("title").style.color = "green"
+    document.getElementById("title").
     innerHTML = "Hello World!"
+}
+count = 0
+function change(){
+    
+    document.getElementById("fn").src = images [count]
+    if(count === 2){
+        count = 0
+    }else{
+        count+=1
+    }
+}
+
+function highlight(){
+    document.getElementById("Btn").style.backgroundColor = "orange"
+    document.getElementById("Btn").style.backgroundColor = "white"
+}
+function normal(){
+     document.getElementById("Btn").style.backgroundColor = "gray"
+    document.getElementById("Btn").style.backgroundColor = "black"
 }
