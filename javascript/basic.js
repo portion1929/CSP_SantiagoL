a734a018a79db87b63c8f6c170de5960b47df636
portion1929 <santiago.lagarde@ucas-edu.net>
2025-04-23 09:51:25 -0600

    asdadadsa

--- javascript/basic.js
+++ javascript/basic.js
@@ -1,33 +1,34 @@
-let images = ["https://m.media-amazon.com/images/I/61Jr-rvLUDL._UF1000,1000_QL80_.jpg"]
-
-
+let images = ["https://www.earthdiver.com/cdn-cgi/image/width=4000,quality=75,format=auto/https://assets.earthdiver.com/media/media-2727684.jp?w=5246&h=2193&tick=1667491652923","https://images.squarespace-cdn.com/content/v1/63c60d44c98af85334537583/644359f9-c213-4d77-bdb0-b78b7f8424f3/Mount_Timpanogos_at_sunset.jpg","https://www.nicodebarmore.com/wp-content/uploads/2014/08/wildflower-basin-1024x1020.jpg"]
 document = notes.html
 function hello(){
-    let name = window.prompt("what is your JM rank")
-    document.getElementById("title").innerHTML = "hello " + name + "!"
+    let name = window.prompt("What is your name?","Koro Sensei")
+    document.getElementById("title").
+    innerHTML = "Hello " + name + "!"
+
 }
 count = 0
 function change(){
-    
-    document.getElementById("fn").src = images [count]
+    document.getElementById("img").src= images[count]
     if(count === 2){
         count = 0
     }else{
-        count+=1
+        count +=1
     }
 }
-
 function highlight(){
-    document.getElementById("Btn").style.backgroundColor = "orange"
-    document.getElementById("Btn").style.backgroundColor = "white"
+    document.getElementById("btn").style.backgroundColor = "orange"
+    document.getElementById("btn").style.color = "white"
 }
 function normal(){
-     document.getElementById("Btn").style.backgroundColor = "gray"
-    document.getElementById("Btn").style.backgroundColor = "black"
+    document.getElementById("btn").style.backgroundColor="gray"
+    document.getElementById("btn").style.color = "black"
+}
+function push(){
+    document.getElementById("btn").style.backgroundColor="red"
 }
 function show(){
     document.getElementById("hidden").style.display = "block"
 }
 function pop(){
-    window.alert("DONT CLICK IT RETARD")
+    window.alert("For real. Don't click this!")
 }
