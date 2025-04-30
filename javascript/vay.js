7b6630484c5dfc06aba7c03f0636001d1323c828
portion1929 <santiago.lagarde@ucas-edu.net>
2025-04-30 09:34:17 -0600

    asdasdsda

--- javascript/vay.js
+++ javascript/vay.js
@@ -3,8 +3,10 @@ function show(){
     if(document.getElementById("hist").style.display != "flex")
         {document.getElementById("hist").style.display = "flex"
     document.getElementById("hist").innerHTML = "Sand Hollow Reservoir, completed in March 2002, is a key water resource for Washington County, Utah, and a popular recreation destination. It was built primarily for managed aquifer recharge, a system where surface water is diverted into an underground aquifer for later retrieval. The reservoir also provides significant recreational opportunities, making Sand Hollow State Park one of Utah's most visited destinations."
-    }else{
+        document.getElementById("hidden").style.display = "block"
+}else{
         document.getElementById("hist").style.display = "none"
     document.getElementById("hist").innerHTML = "show more"
+        document.getElementById("hidden").style.display = "none"
     }
 }
