db8869ba1954cf502c50d61f345db1eed571956f
portion1929 <santiago@ucas-edu.net>
2025-05-05 19:53:29 -0600

    asd

--- javascript/vay.js
+++ javascript/vay.js
@@ -1,6 +1,6 @@
 document.vayctaion.html
 
-let images = ["https://www.stgeorgeutahvacationrentals.com/media/654141a79e1a1eee2ee65b49/xlarge.webp","https://www.thespectrum.com/gcdn/presto/2022/02/05/PSTG/d8d7309c-96ed-466a-8fce-e44c19714c37-STG_0205_Sand_Hollow_Skyfest_85.jpg","https://images.ctfassets.net/0wjmk6wgfops/7ulinJMgm6hCMcbeTZTOff/064d72d46cc9f7fe9cee459ff8022962/f17db71139_50c84de3-ea1c-4298-8341-6da4aa17a979.jpg?w=1280&h=800&fit=fill&f=center&q=80"]
+let images = ["https://www.stgeorgeutahvacationrentals.com/media/654141a79e1a1eee2ee65b49/xlarge.webp","https://www.thespectrum.com/gcdn/presto/2022/02/05/PSTG/d8d7309c-96ed-466a-8fce-e44c19714c37-STG_0205_Sand_Hollow_Skyfest_85.jpg"]
 
 function show(){
     if(document.getElementById("hist").style.display != "flex")
@@ -13,9 +13,9 @@ function show(){
         document.getElementById("hidden").style.display = "none"
     }
 }
-count = 0 
+count = 0
 function change(){
-    document.getElementById("fixit").src=images[count]
+    document.getElementById("sid").src=images[count]
     if(count === 1){
         count = 0
     }else{
