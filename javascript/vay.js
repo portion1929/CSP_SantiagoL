b69bd201d7df38f283a67855e062f0aca5b74ae5
portion1929 <santiago@ucas-edu.net>
2025-05-05 20:02:00 -0600

    asd

--- javascript/vay.js
+++ javascript/vay.js
@@ -1,6 +1,6 @@
 document.vayctaion.html
 
-let images = ["https://www.stgeorgeutahvacationrentals.com/media/654141a79e1a1eee2ee65b49/xlarge.webp","https://www.thespectrum.com/gcdn/presto/2022/02/05/PSTG/d8d7309c-96ed-466a-8fce-e44c19714c37-STG_0205_Sand_Hollow_Skyfest_85.jpg"]
+let image = ["https://www.stgeorgeutahvacationrentals.com/media/654141a79e1a1eee2ee65b49/xlarge.webp","https://www.thespectrum.com/gcdn/presto/2022/02/05/PSTG/d8d7309c-96ed-466a-8fce-e44c19714c37-STG_0205_Sand_Hollow_Skyfest_85.jpg"]
 
 function show(){
     if(document.getElementById("hist").style.display != "flex")
@@ -13,9 +13,10 @@ function show(){
         document.getElementById("hidden").style.display = "none"
     }
 }
+
 count = 0
 function change(){
-    document.getElementById("sid").src=images[count]
+    document.getElementById("fixit").src=image[count]
     if(count === 1){
         count = 0
     }else{
